33560fb73d1e0bb293554bf5556e7645ff40e3cd
vesagashi12 <vesa.gashi@digitalschool.tech>
2024-07-04 11:22:03 +0200

    Module 4

---
+++ Module 4/challengePractice.py
@@ -0,0 +1,14 @@
+# Make sure before the class start to make a thorough review on conditionals
+# But just to practice
+# more try doing a challenge with your students
+
+a = int(input("Write a:"))
+b = int(input("Write b: "))
+
+
+if a > b:
+    print("a is greater than b.")
+elif a < b:
+    print("a is less than b.")
+else:
+    print("a and b are equal.")

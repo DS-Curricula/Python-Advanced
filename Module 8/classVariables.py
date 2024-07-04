0e4425769e63b036bec48e451a5a89084283a2cb
vesagashi12 <vesa.gashi@digitalschool.tech>
2024-07-04 16:57:35 +0200

    Module 8

---
+++ Module 8/classVariables.py
@@ -0,0 +1,8 @@
+class Student:
+    school_name = "Digital School" # This is a class variable
+
+#Create an instance of the Student class
+student_1 = Student()
+
+#Access and print the value of the school_name class variable using the instance
+print(student_1.school_name) #Output:  Digital School

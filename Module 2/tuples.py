326700eed32dd99fe83a672c4b6ffda33b5c6235
puhize <puhizarexha@hotmail.com>
2024-07-05 12:52:21 +0200

    added tuple unpacking example

--- Module 2/tuples.py
+++ Module 2/tuples.py
@@ -11,3 +11,10 @@ print(empty_tuple)
 # Try to reassign a value and ask the student about their output (they would get an error)
 # words[1] = "cheese"
 
+
+# Creating a tuple with information about a person
+person = ("Alice", 30, "Engineer")
+# Tuple unpacking
+name, age, profession = person
+# Printing the information about person
+print(name, "'s", " profession is", profession, "and she is", age, "years old.")

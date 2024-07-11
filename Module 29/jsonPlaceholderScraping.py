1e3e166ce5295f20a8bd7474c4e56b17cb196eb0
vesagashi12 <vesa.gashi@digitalschool.tech>
2024-07-11 16:41:42 +0200

    Delete jsonPlaceholderScraping.py

--- Module 29/jsonPlaceholderScraping.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://jsonplaceholder.typicode.com/todos/1"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-}
-
-response = requests.get(url, headers=headers)
-print(response.text)

84c224b4ee1056f9bdba303701be8661a396424f
puhize <puhizarexha@hotmail.com>
2024-07-23 16:34:41 +0200

    changed the challenge2.py

--- Module 2/challenge2.py
+++ Module 2/challenge2.py
@@ -1,19 +1,17 @@
 # Initial dictionary of books
 books = {
-    "1984": ("George Orwell", "Dystopian"),
-    "To Kill a Mockingbird": ("Harper Lee", "Classic"),
-    "The Great Gatsby": ("F. Scott Fitzgerald", "Classic")
+    ("1984", "George Orwell"): "Dystopian",
+    ("To Kill a Mockingbird", "Harper Lee"): "Classic",
+    ("The Great Gatsby", "F. Scott Fitzgerald"): "Classic"
 }
 
 # Add a new book named "Brave New World"
-books["Brave New World"] = ("Aldous Huxley", "Dystopian")
+books[("Brave New World", "Aldous Huxley")] = "Dystopian"
 
-# Retrieve and print the author and genre of "1984"
-book_info = books["1984"]
-print("1984's author:", book_info[0])
-print("1984's genre:", book_info[1])
+# Retrieve and print the genre of "1984"
+book_info = books[("1984", "George Orwell")]
+print("1984's genre:", book_info)
 
-# Retrieve and print the author and genre of "Brave New World"
-book_info = books["Brave New World"]
-print("Brave New World's author:", book_info[0])
-print("Brave New World's genre:", book_info[1])
+# Retrieve and print the genre of "Brave New World"
+book_info = books[("Brave New World", "Aldous Huxley")]
+print("Brave New World's genre:", book_info)

cca7f67e3f2aeb269ba1eee2720ea4f9363b3244
donika <donika.pllana01@gmail.com>
2024-08-19 11:33:01 +0200

    updated source code

--- Module22/user_models.py
+++ Module22/user_models.py
@@ -11,34 +11,33 @@ from typing import Optional
 
 
 
-# Define the User class with optional fields
+# Modifying the previous model
 class User(BaseModel):
     id: int
     name: str
-    age: Optional[int] = None
-    email: Optional[str] = None
+    age: int = 0  # Default value if age is not provided
+    email: str = "noemail@example.com" # Default value if email is not provided
+
+# Create instances of User and print them
+user1 = User(id=2, name="Jane Doe", email="janedoe@example.com")
+print(user1)
+
+user2 = User(id=3, name="Alice", age=25)
+print(user2)
+
+user4 = User(id=4, name="Bob")
+print(user4)
 
 # Define another_user class with constraints
 class another_user(BaseModel):
     id: conint(gt=0)  # id must be greater than 0
     name: constr(min_length=2, max_length=50)  # name length must be between 2 and 50
 
-# Create instances of User and print them
-user1 = User(id=1, name="Alice", age=25, email="alice@example.com")
-print(user1)
 
-user2 = User(id=2, name="Bob", email="bob@example.com")
-print(user2)
-
-user3 = User(id=3, name="Charlie", age=30)
-print(user3)
-
-user4 = User(id=4, name="Dana")
-print(user4)
 
 # Create instances of another_user and print them
 valid_user = another_user(id=1, name="Alice")
-print(valid_user)
+#print(valid_user)
 
 # The following instance is invalid and should raise a validation error if uncommented
 # invalid_user1 = another_user(id=0, name="Alice")
